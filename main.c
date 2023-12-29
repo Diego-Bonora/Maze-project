d1283b0458edd5f53ee4986f52f18f8c5b89ab94
Diego-Bonora <bonoradi@gmail.com>
2023-12-29 17:15:58 -0300

    added a function for drawing the grid based on the extern variable

--- main.c
+++ main.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
+int map_grid[16][28];
+
 int main(int argc, char *argv[])
 {
+    generate_map_grid();
+    int WIDTH = 1260, HEIGHT = 720;
+
     SDL_Instance instance;
 
-    if (init_instance(&instance) != 0)
+    if (init_instance(&instance, WIDTH, HEIGHT) != 0)
         return 1;
 
     while (1)
     {
-        SDL_SetRenderDrawColor(instance.renderer, 0, 0, 0, 0);
+        SDL_SetRenderDrawColor(instance.renderer, 255, 255, 255, 255);
         SDL_RenderClear(instance.renderer);
         if (poll_events() == 1)
             break;
         /*
         draws
         */
+        box_struct(instance);
         SDL_RenderPresent(instance.renderer);
     }
     SDL_DestroyRenderer(instance.renderer);
@@ -24,11 +30,42 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-int init_instance(SDL_Instance *instance)
+void box_struct(SDL_Instance instance)
+{
+    int x, y, w = 45, h = 45;
+
+    for (y = 0; y < 16; ++y)
+    {
+        for (x = 0; x < 28; ++x)
+        {
+            if (y % 2 == 0)
+            {
+                if (x % 2 == 0)
+                    SDL_SetRenderDrawColor(instance.renderer, 150, 150, 150, 255); // Set color to red (R, G, B, A)
+                else
+                    SDL_SetRenderDrawColor(instance.renderer, 120, 120, 120, 255); // Set color to red (R, G, B, A)
+            }
+            else
+            {
+                if (x % 2 != 0)
+                    SDL_SetRenderDrawColor(instance.renderer, 150, 150, 150, 255); // Set color to red (R, G, B, A)
+                else
+                    SDL_SetRenderDrawColor(instance.renderer, 120, 120, 120, 255); // Set color to red (R, G, B, A)
+            }
+            if (map_grid[y][x] == 1)
+            {
+                SDL_Rect box[1] = {x * 45, y * 45, w, h};
+                SDL_RenderFillRect(instance.renderer, &box[0]);
+            }
+        }
+    }
+}
+
+int init_instance(SDL_Instance *instance, int WIDTH, int HEIGHT)
 {
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    instance->window = SDL_CreateWindow("Hello world!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1260, 720, SDL_WINDOW_ALLOW_HIGHDPI);
+    instance->window = SDL_CreateWindow("Hello world!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
     if (instance->window == NULL)
     {
         fprintf(stderr, "Could not create window: %s\n", SDL_GetError());
