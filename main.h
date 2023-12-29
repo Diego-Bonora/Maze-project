0ab359311b524b653684ba4b318f05b2abac3c4e
Diego-Bonora <bonoradi@gmail.com>
2023-12-29 17:14:38 -0300

    new funcions and an external variable added

--- main.h
+++ main.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <math.h>
+
+extern int map_grid[16][28];
 
 typedef struct SDL_Instance
 {
@@ -10,6 +13,9 @@ typedef struct SDL_Instance
     SDL_Renderer *renderer;
 } SDL_Instance;
 
-int init_instance(SDL_Instance *instance);
+int init_instance(SDL_Instance *instance, int WIDTH, int HEIGHT);
 int poll_events();
+void box_struct(SDL_Instance instance);
+int **generate_map_grid(void);
+
 #endif
