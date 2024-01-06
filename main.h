8321b4d4d78a48dc5f227d7267f13a6b00405eec
Diego-Bonora <bonoradi@gmail.com>
2024-01-06 14:45:12 -0300

    added new functions to the project

--- main.h
+++ main.h
@@ -6,7 +6,9 @@
 #include <SDL2/SDL_ttf.h>
 #include <math.h>
 
-extern int map_grid[16][28];
+extern float PI, PI2, PI3, DR;
+extern int map_grid[16 * 28];
+extern int All_Textures[4090];
 
 /*
 Struct for the main instance variables
@@ -27,12 +29,11 @@ struct Player
 {
     int size;
     int movement_speed;
-    int pos_x;
-    int pos_y;
-    int top_left[2];
-    int top_right[2];
-    int bottom_left[2];
-    int bottom_right[2];
+    float pos_x;
+    float pos_y;
+    double player_angle;
+    float delta_x;
+    float delta_y;
 };
 
 extern struct Player square;
@@ -49,9 +50,16 @@ int wall_color(int num, SDL_Instance instance);
 /*player.c functions*/
 void player_position(SDL_Instance instance);
 int win(SDL_Instance instance);
+void movements(void);
 void move_up(void);
 void move_down(void);
-void move_left(void);
-void move_right(void);
+void rotate_left(void);
+void rotate_right(void);
+void hitbox(SDL_Instance instance);
+
+/* 3D rendering */
+void draw_rays(SDL_Instance instance);
+float dist(float ax, float ay, float bx, float by, float ang);
+float FixAng(float a);
 
 #endif
