e0d52d6475ecaa5a10ebc366a05791d2682dc42b
Diego-Bonora <bonoradi@gmail.com>
2024-01-06 14:57:15 -0300

    removed all the bounding box positions, centered the texture, added rotation and moved all the key states to this file, added hitbox function that will spawn a red box arround the player showing the colition

--- player.c
+++ player.c
@@ -6,20 +6,14 @@ This function spawns the player in the correct position and resets the bounding 
 
 void player_position(SDL_Instance instance)
 {
-    int extra_pixel = square.size - 1;
-
-    /* resets the four corners bounding box using x and y position of the player */
-    square.top_left[1] = square.pos_x, square.top_left[0] = square.pos_y;
-    square.top_right[1] = square.pos_x + extra_pixel, square.top_right[0] = square.pos_y;
-    square.bottom_left[1] = square.pos_x, square.bottom_left[0] = square.pos_y + extra_pixel;
-    square.bottom_right[1] = square.pos_x + extra_pixel, square.bottom_right[0] = square.pos_y + extra_pixel;
 
     /* generates the player using the .bmp file and rendering it */
     SDL_Surface *image = SDL_LoadBMP("player.bmp");
     SDL_Texture *img_texture = SDL_CreateTextureFromSurface(instance.renderer, image);
-    SDL_Rect image_rect[1] = {square.pos_x, square.pos_y, square.size, square.size};
+    SDL_Rect image_rect[1] = {square.pos_x - square.size / 2, square.pos_y - square.size / 2, square.size, square.size};
 
-    SDL_RenderCopy(instance.renderer, img_texture, NULL, image_rect);
+    SDL_RenderCopyEx(instance.renderer, img_texture, NULL, image_rect, (square.player_angle * 180 / PI), NULL, SDL_FLIP_NONE);
+    hitbox(instance);
 
     SDL_FreeSurface(image);
     SDL_DestroyTexture(img_texture);
@@ -32,7 +26,7 @@ When the game is over the text stays on the window until the game its closed and
 
 int win(SDL_Instance instance)
 {
-    if (map_grid[square.pos_y / 45][square.pos_x / 45] == 3)
+    if (map_grid[(int)square.pos_y / 45 * 28 + (int)square.pos_x / 45] == 3)
     {
 
         TTF_Init();
@@ -73,35 +67,72 @@ Start of functions for the movement of the player
 This functions utilizes the bounding box to check if the player can move or if its hitting any wall
 The positions are divided by 45 because each block measures 45*45px and by dividing it you can check the players position iside the map grid
 */
+const Uint8 *KeyState;
+
+void movements(void)
+{
+    KeyState = SDL_GetKeyboardState(NULL); /* state checker for the player movement */
+    if (KeyState[SDL_SCANCODE_W] || KeyState[SDL_SCANCODE_UP])
+        move_up();
+    if (KeyState[SDL_SCANCODE_S] || KeyState[SDL_SCANCODE_DOWN])
+        move_down();
+    if (KeyState[SDL_SCANCODE_A] || KeyState[SDL_SCANCODE_LEFT])
+        rotate_left();
+    if (KeyState[SDL_SCANCODE_D] || KeyState[SDL_SCANCODE_RIGHT])
+        rotate_right();
+}
+
+void rotate_left(void)
+{
+    square.player_angle -= 0.1;
+    if (square.player_angle < 0)
+        square.player_angle += 2 * PI;
+    square.delta_x = cos(square.player_angle) * square.movement_speed;
+    square.delta_y = sin(square.player_angle) * square.movement_speed;
+}
+
+void rotate_right(void)
+{
+    square.player_angle += 0.1;
+    if (square.player_angle > 2 * PI)
+        square.player_angle -= 2 * PI;
+    square.delta_x = cos(square.player_angle);
+    square.delta_y = sin(square.player_angle);
+}
 
 void move_up(void)
 {
-    if (map_grid[(square.top_left[0] - square.movement_speed) / 45][square.top_left[1] / 45] != 1 &&
-        map_grid[(square.top_right[0] - square.movement_speed) / 45][square.top_right[1] / 45] != 1)
-        square.pos_y -= square.movement_speed;
+    int x_offset = (square.delta_x < 0) ? -5 : 5;
+    int y_offset = (square.delta_y < 0) ? -5 : 5;
+
+    if (map_grid[(int)(square.pos_y / 45) * 28 + (int)((square.pos_x + x_offset) / 45)] != 1)
+        square.pos_x += square.delta_x;
+    if (map_grid[(int)((square.pos_y + y_offset) / 45) * 28 + (int)(square.pos_x / 45)] != 1)
+        square.pos_y += square.delta_y;
 }
 
 void move_down(void)
 {
-    if (map_grid[(square.bottom_left[0] + square.movement_speed) / 45][square.bottom_left[1] / 45] != 1 &&
-        map_grid[(square.bottom_right[0] + square.movement_speed) / 45][square.bottom_right[1] / 45] != 1)
-        square.pos_y += square.movement_speed;
-}
+    int x_offset = (square.delta_x < 0) ? -5 : 5;
+    int y_offset = (square.delta_y < 0) ? -5 : 5;
 
-void move_left(void)
-{
-    if (map_grid[square.top_left[0] / 45][(square.top_left[1] - square.movement_speed) / 45] != 1 &&
-        map_grid[square.bottom_left[0] / 45][(square.bottom_left[1] - square.movement_speed) / 45] != 1)
-        square.pos_x -= square.movement_speed;
-}
-
-void move_right(void)
-{
-    if (map_grid[square.top_right[0] / 45][(square.top_right[1] + square.movement_speed) / 45] != 1 &&
-        map_grid[square.bottom_right[0] / 45][(square.bottom_right[1] + square.movement_speed) / 45] != 1)
-        square.pos_x += square.movement_speed;
+    if (map_grid[(int)(square.pos_y / 45) * 28 + (int)((square.pos_x - x_offset) / 45)] != 1)
+        square.pos_x -= square.delta_x;
+    if (map_grid[(int)((square.pos_y - y_offset) / 45) * 28 + (int)(square.pos_x / 45)] != 1)
+        square.pos_y -= square.delta_y;
 }
 
 /*
 End of functions for the movement of the player
 */
+
+/* this function renders a red box arround the player when playing on 2D map */
+void hitbox(SDL_Instance instance)
+{
+    int distance = 5;
+    SDL_SetRenderDrawColor(instance.renderer, 255, 0, 0, 255);
+    SDL_RenderDrawLine(instance.renderer, square.pos_x - distance, square.pos_y - distance, square.pos_x - distance, square.pos_y + distance);
+    SDL_RenderDrawLine(instance.renderer, square.pos_x - distance, square.pos_y - distance, square.pos_x + distance, square.pos_y - distance);
+    SDL_RenderDrawLine(instance.renderer, square.pos_x - distance, square.pos_y + distance, square.pos_x + distance, square.pos_y + distance);
+    SDL_RenderDrawLine(instance.renderer, square.pos_x + distance, square.pos_y - distance, square.pos_x + distance, square.pos_y + distance);
+}
